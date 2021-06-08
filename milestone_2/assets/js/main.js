e11f186ff0914e392b3b9ccdce1724e712f059c2
valeriomatranga <matrangav@gmail.com>
2021-06-09 01:03:23 +0200

    serie

--- milestone_2/assets/js/main.js
+++ milestone_2/assets/js/main.js
@@ -2,26 +2,37 @@ const app = new Vue({
     el:"#app",
 
     data:{
-        url:"https://api.themoviedb.org/3/search/movie",
+        url:"https://api.themoviedb.org/3/search/",
+        movie:"movie",
+        tv:"tv",
         apikey:"0399f6a1aef262ecdd313571205ae96f",
-        query:"",
+        query: '',
         films:[],
+        serieTv:[]
 
     },
 
     methods:{
         search(){
-            const film = `${this.url}?api_key=${this.apikey}&query=${this.query}`;
+            const film = `${this.url}${this.movie}?api_key=${this.apikey}&query=${this.query}`;
             //console.log(film);
-
             axios
             .get(film)
             .then(resp =>{
                 this.films = resp.data.results
                 console.log(this.films);
+            });
+
+            const tv = `${this.url}${this.tv}?api_key=${this.apikey}&query=${this.query}`;
+            //console.log(tv);
+            axios
+            .get(tv)
+            .then(resp =>{
+                this.serieTv = resp.data.results
+                console.log(this.serieTv);
             })
 
-            
+
 
         }
         
@@ -29,6 +40,11 @@ const app = new Vue({
 
     mounted(){
         this.search()
+        
 
     },
+
+    computed:{
+
+    }
 })
