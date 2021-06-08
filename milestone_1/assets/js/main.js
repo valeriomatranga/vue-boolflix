1051cec950cad2953ed0b551dfc00b9cabf4765f
valeriomatranga <matrangav@gmail.com>
2021-06-08 15:08:28 +0200

    ricerca

--- milestone_1/assets/js/main.js
+++ milestone_1/assets/js/main.js
@@ -2,14 +2,39 @@ const app = new Vue({
     el:"#app",
 
     data:{
+        url:"https://api.themoviedb.org/3/search/movie",
+        apikey:"0399f6a1aef262ecdd313571205ae96f",
+        query:"ritorno",
+        films:[],
+        ricerca:''
 
     },
 
-    computed:{
+    methods:{
+        search(){
+            const ricercaFilms = this.ricerca.toLowerCase();
+            this.films.forEach(film => {
+                const titoloFilm = film.title.toLowerCase();
+                console.log(titoloFilm);
+                if(titoloFilm.includes(ricercaFilms)){
+                    film.visible = true;
+                }else{
+                    film.visible = false;
+                }
+            });
+        }
         
     },
 
     mounted(){
+        const film = `${this.url}?api_key=${this.apikey}&query=${this.query}`;
+        //console.log(film);
 
+        axios
+        .get(film)
+        .then(resp =>{
+            this.films = resp.data.results
+            console.log(this.films);
+        })
     },
 })
